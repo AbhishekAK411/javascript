9932c2a48d2019fa68b6c8ac76d67d401bb921d2
AbhishekAK411 <127575574+AbhishekAK411@users.noreply.github.com>
2023-04-02 18:46:40 +0530

    basics of js

---
+++ server.js
@@ -0,0 +1,53 @@
+// var a = 123;
+// console.log(a);
+// console.log(typeof(a));
+
+// var b = "rinzler";
+// console.log(b);
+// console.log(typeof(b));
+
+// var c = 'rinzler';
+// console.log(c);
+// console.log(typeof(c));
+
+// var d = true;
+// console.log(d);
+// console.log(typeof(d));
+
+// var e;
+// console.log(e);
+// console.log(typeof(e));
+
+
+// var x=9876543;
+// var y="9876543";
+// console.log(x==y); // data only
+// console.log(x===y); // data and datatypes
+
+
+
+
+
+// var a="rinzler";
+// var b="rinzler1";
+
+// if(a==b){
+//     console.log(true);
+// }
+
+// else{
+//     console.log(false);
+// }
+
+
+
+var a;
+var b=56789;
+console.log(a && b);
+console.log(a || b);
+
+
+var x=234;
+var y;
+console.log(x && y);
+console.log(x || y);
