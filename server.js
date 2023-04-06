9ba579018a742a4816ffb746e2cffc51c0c45c45
AbhishekAK411 <127575574+AbhishekAK411@users.noreply.github.com>
2023-04-07 02:01:31 +0530

    added

--- server.js
+++ server.js
@@ -53,22 +53,120 @@
 // console.log(x || y);
 
 
-var userAge =90;
+// var userAge =90;
 
-switch(true){
-    case (userAge > 18 && userAge < 80):
-        console.log("DL Issued");
+// switch(true){
+//     case (userAge > 18 && userAge < 80):
+//         console.log("DL Issued");
+//         break;
+//     case (userAge > 80):
+//         console.log("Overage");
+//         break;
+//     case (userAge == 18):
+//         console.log("LL Issued");
+//         break;
+//     case (userAge < 18):
+//         console.log("Underage");
+//         break;
+//     default:
+//         console.log();
+//         break;
+// }
+
+
+// var dayNumber = 12;
+
+// switch (dayNumber) {
+//   case 1:
+//     console.log("Sunday");
+//     break;
+//   case 2:
+//     console.log("Monday");
+//     break;
+//   case 3:
+//     console.log("Tuesday");
+//     break;
+//   case 4:
+//     console.log("Wednesday");
+//     break;
+//   case 5:
+//     console.log("Thursday");
+//     break;
+//   case 6:
+//     console.log("Friday");
+//     break;
+//   case 7:
+//     console.log("Saturday");
+//     break;
+//   default:
+//     console.log("Invalid input!");
+// }
+
+
+// var month = 11;
+
+// switch(month){
+//     case 1:
+//         console.log("31 days");
+//         break;
+//     case 2:
+//         console.log("28/29 days");
+//         break;
+//     case 3:
+//         console.log("31 days");
+//         break;
+//     case 4:
+//         console.log("30 days");
+//         break;
+//     case 5:
+//         console.log("31 days");
+//         break;
+//     case 6:
+//         console.log("30 days");
+//         break;
+//     case 7:
+//         console.log("31 days");
+//         break;
+//     case 8:
+//         console.log("30 days");
+//         break;
+//     case 9:
+//         console.log("31 days");
+//         break;
+//     case 10:
+//         console.log("30 days");
+//         break;
+//     case 11:
+//         console.log("31 days");
+//         break;
+//     case 12:
+//         console.log("30 days");
+//         break;
+//     default:
+//         console.log("Invalid Input");
+//         break;
+// }
+
+
+var alphabet="b";
+
+switch(alphabet){
+    case "a":
+        console.log("a is a vowel");
         break;
-    case (userAge > 80):
-        console.log("Overage");
+    case "e":
+        console.log("e is a vowel");
         break;
-    case (userAge == 18):
-        console.log("LL Issued");
+    case "i":
+        console.log("i is a vowel");
         break;
-    case (userAge < 18):
-        console.log("Underage");
+    case "o":
+        console.log("o is a vowel");
+        break;
+    case "u":
+        console.log("u is a vowel");
         break;
     default:
-        console.log();
+        console.log("consonant");
         break;
 }
