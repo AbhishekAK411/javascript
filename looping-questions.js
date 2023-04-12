a8a2ba302e0da4c71f4bcbe3499ee910eb9fa529
AbhishekAK411 <127575574+AbhishekAK411@users.noreply.github.com>
2023-04-12 10:20:49 +0530

    added

--- looping-questions.js
+++ looping-questions.js
@@ -147,3 +147,124 @@
 
 // console.log(array.length);
 
+
+// 8. Number is prime or not
+
+// var prime=3;
+
+// function isPrime(num){
+//     if(num<2){
+//         return false;
+//     }
+//     for(i=2;i<num;i++){
+//         if(num % i===0){
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+// var ans = isPrime(prime);
+// console.log(ans);
+
+
+// 9. calculate HCF of two numbers
+
+// function calcHCF(num1, num2){
+//     let smallNum = Math.min(num1, num2);
+//     let largeNum = Math.max(num1, num2);
+//     let remainder= largeNum % smallNum;
+
+//     while (remainder!=0){
+//         largeNum = smallNum;
+//         smallNum = remainder;
+//         remainder = largeNum % smallNum;
+//     }
+
+//     return smallNum;
+// }
+
+// var ans = calcHCF(24,36);
+// console.log(ans);
+
+
+// 10. create do-while loop. Numbers should be added and sum is to be displayed till user says no.
+
+// 11. for the given range, count the number of positive, negative and number of zeroes
+
+// var a = [1,2,5,-10,0,0,4,10,20];
+// var countPositive=0;
+// var countNegative=0;
+// var countZero=0;
+
+// function countNo(num){
+//     for(i=0;i<=8;i++){
+//         if(num[i]>0){
+//             countPositive++;
+//         }
+//         else if(num[i]<0){
+//             countNegative++;
+//         }
+//         else{
+//             countZero++;
+//         }   
+//     }
+
+//     console.log(countPositive);
+//     console.log(countNegative);
+//     console.log(countZero);
+// }
+
+// countNo(a);
+
+
+// 12. enter range, at the end, print largest and smallest number in the range.
+
+var a = [1,2,3,10,20,30,100,-10,-55];
+
+function largesmall(num){
+    
+}
+
+
+// 13. armstrong number
+
+// for (let num = 1; num <= 500; num++) {
+//     let sum = 0;
+//     let temp = num;
+//     while (temp > 0) {
+//       let digit = temp % 10;
+//       sum += digit ** 3;
+//       temp = Math.floor(temp / 10);
+//     }
+//     if (sum === num) {
+//       console.log(num);
+//     }
+//   }
+
+
+// 14. fibonacci Series 20 range
+
+// let num1 = 0;
+// let num2 = 1;
+// let nextNum;
+// console.log(num1);
+// console.log(num2);
+// for (let i = 3; i <= 20; i++) {
+//   nextNum = num1 + num2;
+//   console.log(nextNum);
+//   num1 = num2;
+//   num2 = nextNum;
+// }
+
+
+// 15. sum of the series
+
+// let n = 10;
+// let sum = 0;
+// let sign = 1;
+// for (let i = 1; i <= n; i++) {
+//   sum += sign * (1 / i);
+//   sign = -sign;
+// }
+// console.log(sum);
